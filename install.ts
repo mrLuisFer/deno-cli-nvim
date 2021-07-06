fe4b2ddd254acfde01e94a8bb9473c971e2958d4
mrLuisFer <lolesuncrak@gmail.com>
2021-07-06 10:50:58 -0500

    adding colors for the shell text and some print functions

--- install.ts
+++ install.ts
@@ -1,17 +1,32 @@
 const { args, run } = Deno;
 import { clone } from "https://deno.land/x/clone/mod.ts";
-import { print } from "./src/print.ts";
+import {
+  printInfo,
+  print,
+  printError,
+  printWarn,
+  printSucces,
+  printDim,
+} from "./src/print.ts";
 
 const repoUrl = "https://github.com/mrLuisFer/neovim-dotfiles.git";
 
-const build = async () => {
-  print("Clonando Configuracion!");
+const init = async () => {
+  printDim("⚡ Welcome to the CLI to clone mrLuisFer configuration!");
+  print("🦄 Commands");
+
+  // Test
+  printWarn("some");
+
+  // prompt("Write a command:");
+
+  // print("Clonando Configuracion!");
   print(args);
 
-  await clone(repoUrl, "./cloned");
+  // await clone(repoUrl, "./cloned");
 
   // const p = run({ cmd: [clone, repoUrl] });
   // console.log(p);
 };
 
-build();
+init();
--- src/print.ts
+++ src/print.ts
@@ -1,1 +1,18 @@
+import * as Colors from "https://deno.land/std@0.100.0/fmt/colors.ts";
+
 export const print = (param: any) => console.log(param);
+
+export const printInfo = (param: any) => print(Colors.blue(param));
+
+export const printError = (param: any) => print(Colors.bgRed(param));
+
+export const printWarn = (param: any) => print(Colors.yellow(param));
+
+export const printSucces = (param: any) => print(Colors.green(param));
+
+/**
+ * The text emits only a small amount of light.
+ * @param {string} param - str text to dim
+ * @returns {void}
+ */
+export const printDim = (param: string) => print(Colors.dim(param));
